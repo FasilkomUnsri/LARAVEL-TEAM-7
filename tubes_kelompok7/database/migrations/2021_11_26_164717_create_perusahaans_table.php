9ec94f5335181a38754bacdacd956742d804bcd4
Marita Santi <maritaati8@gmail.com>
2021-11-27 00:38:04 +0700

    cobacrud

---
+++ tubes_kelompok7/database/migrations/2021_11_26_164717_create_perusahaans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerusahaansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perusahaans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('jenis_kelamin');
+            $table->string('tempat_kelahiran');
+            $table->string('tanggal_lahir');
+            $table->text('alamat');
+            $table->string('jabatan');
+            $table->string('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perusahaans');
+    }
+}
